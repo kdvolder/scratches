f2c322c3978d1f173bac2e4896e68dd4c0978a95
Kris De Volder <kdevolder@pivotal.io>
2015-01-08 09:58:05 -0800

    Sample project 'demo-live-metadata-incremental'.
    Sample project for issue gh-2313. Demonstrates that annotation
    processor produces incomplete information for incremental
    builds.

---
+++ demo-live-metadata-incremental/src/main/java/demo/BarProperties.java
@@ -0,0 +1,54 @@
+package demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ *
+ * @author Stephane Nicoll
+ */
+@ConfigurationProperties("bar")
+public class BarProperties {
+
+	private String name;
+
+	private String description;
+	
+	/**
+	 * A nice counter description.
+	 */
+	private Integer counter = 0;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getCounter() {
+		return counter;
+	}
+
+	public void setCounter(Integer counter) {
+		this.counter = counter;
+	}
+
+//	private String bar;
+//
+//	public String getBar() {
+//		return bar;
+//	}
+//
+//	public void setBar(String bar) {
+//		this.bar = bar;
+//	}
+}
